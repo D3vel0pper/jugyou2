1dbf4f8a5a50bc65b01cfc1423fa5e299aa86f3a
Takahiro Suzuki <takioyaro@live.jp>
2015-11-05 09:49:51 +0900

    FizzBuzz fixed

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -1,7 +1,9 @@
 <?php
   $i = 1;
   for(;$i<=100;$i++){
-    if($i%3 == 0){
+    if($i%15 == 0){
+      print "FizzBuzz</br>";
+    }else if($i%3 == 0){
       print "Fizz</br>";
     }else if($i%5 == 0){
       print "Buzz</br>";
