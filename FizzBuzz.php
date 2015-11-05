35a2305b710a3f40e5a68d2f8642ed4514a7b633
Takahiro Suzuki <takioyaro@live.jp>
2015-11-05 09:47:06 +0900

    Buzz added

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -3,7 +3,9 @@
   for(;$i<=100;$i++){
     if($i%3 == 0){
       print "Fizz</br>";
-    }else{
+    }else if($i%5 == 0){
+      print "Buzz</br>";
+    } else{
       print "{$i}</br>";
     }
   }
